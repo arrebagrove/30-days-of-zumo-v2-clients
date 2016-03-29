cc608d9d806aa36ae3416446a1e8e7a2677fec0a
Adrian Hall <adrian@shellmonger.com>
2016-03-29 15:43:40 -0700

    Added client.token

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -52,6 +52,8 @@
      * Called after the entry button is clicked to clean up the old HTML and add our HTML
      */
     function initializeApp() {
+        console.log('client token = ', client.currentUser);
+
         $('#wrapper').empty();
 
         // Replace the wrapper with the main content from the original Todo App
