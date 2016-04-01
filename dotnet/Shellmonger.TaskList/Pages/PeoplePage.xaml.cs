293199cd51efb19a6263895a1478ffee8917bd0f
Adrian Hall <adrian@shellmonger.com>
2016-04-01 14:44:34 -0700

    Main navigation

---
+++ dotnet/Shellmonger.TaskList/Pages/PeoplePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+
+namespace Shellmonger.TaskList.Pages
+{
+    public sealed partial class PeoplePage : Page
+    {
+        public PeoplePage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event Handler: Log Out
+        /// </summary>
+        private void HangUp_Click(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(EntryPage));
+        }
+
+        /// <summary>
+        /// Event Handler: Clicked on People
+        /// </summary>
+        private void Tasks_Click(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(TasksPage));
+        }
+
+        private async void RefreshIcon_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            RefreshIconRotation.Begin();
+            RefreshIcon.IsTapEnabled = false;
+
+            // TODO: Processing
+            await Task.Delay(2000);
+
+            RefreshIcon.IsTapEnabled = true;
+            RefreshIconRotation.Stop();
+        }
+    }
+}
---
+++ dotnet/Shellmonger.TaskList/Pages/TasksPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+
+namespace Shellmonger.TaskList.Pages
+{
+    public sealed partial class TasksPage : Page
+    {
+        public TasksPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event Handler: Log Out
+        /// </summary>
+        private void HangUp_Click(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(EntryPage));
+        }
+
+        /// <summary>
+        /// Event Handler: Clicked on People
+        /// </summary>
+        private void People_Click(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(PeoplePage));
+        }
+
+        private async void RefreshIcon_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            RefreshIconRotation.Begin();
+            RefreshIcon.IsTapEnabled = false;
+
+            // TODO: Processing
+            await Task.Delay(2000);
+
+            RefreshIcon.IsTapEnabled = true;
+            RefreshIconRotation.Stop();
+        }
+    }
+}
